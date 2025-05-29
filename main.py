d542525deac52f9bf8b7c093a45235ab0273dcb9
085_Silva Oktaria Putri <145788056+Silvok@users.noreply.github.com>
2025-05-29 14:23:29 +0700

    penambahan keterangan

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-import pygame # Import pygame to handle quit outside Game class
+import pygame #import pygame untuk mengakses fungsi pygame
 
+#import class Game dari file game.py
 from game import Game
 
+#titik masuk untuk program
 if __name__ == '__main__':
-    play_again = True
+    play_again = True # flag untuk menentukan apakah permainan akan diulang
+    # loop untuk menjalankan permainan
     while play_again:
-        game = Game()
-        if game.is_running: # Only run if webcam initialized successfully
-            play_again = game.run() # game.run() now returns True for play again, False for quit
-        else: # If game couldn't even start (e.g., webcam error)
+        game = Game() # buat instance dari class Game
+        #Loop utama program - akan terus berjalan selama permainan masih berjalan
+        if game.is_running:
+            # Jalankan game, yang akan mengatur semua aspek permainan
+            play_again = game.run() 
+        else: 
+            # Jika permainan tidak berjalan, setel play_again ke False
             play_again = False # Don't try to play again
 
-    pygame.quit() # Ensure pygame is quit after all game sessions are done
+    pygame.quit() #setelah semua permainan selesai, keluar dari pygame
